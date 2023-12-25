99680c99e0a5193210f95b9906334cced61d05a8
chenjie <8889582+chenjie55@user.noreply.gitee.com>
2023-12-25 16:54:13 +0800

    test 1

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-from main import loadData
-loadData()
+import pytest
+
+def fib(n):
+    if n <= 1:
+        return 1
+    return fib(n - 2) + fib(n - 1)
+
+def test_fib_10(benchmark):
+    benchmark(fib, 10)
+
+def test_fib_20(benchmark):
+    benchmark(fib, 20)

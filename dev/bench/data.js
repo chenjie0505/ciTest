5cc3ee51494c53661dbf1509e5f1d717cde3b555
github-action-benchmark <github@users.noreply.github.com>
2023-12-26 03:22:57 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for cb80105f0366e8afe9458609c321e545f83e9ca7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1703560004125,
+  "lastUpdate": 1703560977497,
   "repoUrl": "https://github.com/chenjie0505/ciTest",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000012908553338033844",
             "extra": "mean: 13.45336171772613 usec\nrounds: 61313"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "8889582+chenjie55@user.noreply.gitee.com",
+            "name": "chenjie"
+          },
+          "committer": {
+            "email": "8889582+chenjie55@user.noreply.gitee.com",
+            "name": "chenjie"
+          },
+          "distinct": true,
+          "id": "cb80105f0366e8afe9458609c321e545f83e9ca7",
+          "message": "test",
+          "timestamp": "2023-12-26T11:22:20+08:00",
+          "tree_id": "999de5f43ccf0763e4b826b21d9e425ceb50770b",
+          "url": "https://github.com/chenjie0505/ciTest/commit/cb80105f0366e8afe9458609c321e545f83e9ca7"
+        },
+        "date": 1703560977113,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test.py::test_fib_10",
+            "value": 881520.2452842997,
+            "unit": "iter/sec",
+            "range": "stddev: 1.1235519787359601e-7",
+            "extra": "mean: 1.1344038952587974 usec\nrounds: 191939"
+          },
+          {
+            "name": "test.py::test_fib_20",
+            "value": 6825.709928950853,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000004279436360934027",
+            "extra": "mean: 146.5049072417446 usec\nrounds: 6145"
           }
         ]
       }
